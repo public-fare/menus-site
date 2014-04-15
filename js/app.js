267a5bd025b3ddef8c2420819784e8240d1747f1
Trevor Muñoz <trevor.munoz@gmail.com>
2014-04-15 11:16:54 -0400

    More javascripting

--- js/app.js
+++ js/app.js
@@ -1,12 +1,18 @@
 $(document).ready(function(){
     $.bigfoot();
+
+    var targetEl = $("#article-navbar-collapse-1 > ul");
     var headings = $(".section-head");
     var scrollIds = [];
+    var headingText = [];
     headings.each(function() {
-        scrollIds.push(this.id)
-    ;});
+        scrollIds.push($(this).id);
+        headingText.push($(this).text());
+    });
+
     $.scrollDepth({
         elements: scrollIds,
         userTiming: false
     });
+
 });
