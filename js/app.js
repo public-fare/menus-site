78b02a1deb059968fe3505166ce6b585c04a1d2e
Trevor Muñoz <trevor.munoz@gmail.com>
2014-04-15 07:51:03 -0400

    Fixing semicolons

--- js/app.js
+++ js/app.js
@@ -1,10 +1,10 @@
 $(document).ready(function(){
     $.bigfoot();
-    var headings = $(".section-head")
-    var scrollIds = []
+    var headings = $(".section-head");
+    var scrollIds = [];
     headings.each(function() {
         scrollIds.push(this.id)
-    })
+    ;});
     $.scrollDepth({
         elements: scrollIds,
         userTiming: false
